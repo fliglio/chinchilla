0f41b6f5f40deda8318b942bd268ed72abe169d5
Philip W <philip.whitt@sbcglobal.net>
2015-09-23 18:17:58 -0500

    Adding support for yml collections

--- Fliglio/Chinchilla/Application.php
+++ Fliglio/Chinchilla/Application.php
@@ -4,40 +4,46 @@ namespace Fliglio\Chinchilla;
 
 use SensioLabs\Consul as consul;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Dumper;
 
 class Application {
 
 	const KV_PATH = 'chinchilla/endpoints/';
 
 	private $config;
-	private $rawContents;
+	private $kv;
 
-	public function __construct($configPath = null) {
-		$configPaths = [
-			$configPath,
-			__DIR__ . '/../../chinchilla.yml',
-		];
-
-		foreach ($configPaths as $path) {
-			if (is_file($path)) {
-				$this->rawContents = file_get_contents($path);
-				$this->config = Yaml::parse($this->rawContents);
-				break; // first come first serve
-			}
-		}
-
-		if (!isset($this->config)) {
+	public function __construct($configPath) {
+		if (!is_file($configPath)) {
 			throw new \Exception('chinchilla.yml not found.');
 		}
+
+		$this->config = Yaml::parse(file_get_contents($configPath));
+
+		$sf = new consul\ServiceFactory();
+		$this->kv = $sf->get('kv');
 	}
 
 	public function run() {
-		$name = $this->config['name'].'.yml';
+		$output = '';
 
-		$sf = new consul\ServiceFactory();
-		$kv = $sf->get('kv');
+		if (isset($this->config['endpoints'])) {
+			foreach ($this->config['endpoints'] as $config) {
+				$output .= $this->register($config);
+			}
 
-		$kv->put(self::KV_PATH.$name, $this->rawContents);
+		} else {
+			$output = $this->register($this->config);
+		}
+
+		return $output;
+	}
+
+	private function register($config) {
+		$name = $config['name'].'.yml';
+		$yml = (new Dumper())->dump($config, 2); // indent level 2 is standard yml format 
+
+		$this->kv->put(self::KV_PATH.$name, $yml);
 
 		return sprintf("\tInstalled chinchilla endpoint: %s\n", $name);
 	}
